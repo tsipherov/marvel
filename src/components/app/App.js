74c7d22a58cf0fa152de30b46860eb67376a0260
tsipherov <tsipherov@gmail.com>
2023-03-10 22:37:55 +0200

    implemented randomChar component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,10 @@
-import { useEffect } from "react";
-
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-
-import MarvelServices from "../services/MarvelServices";
-
 import decoration from "../../resources/img/vision.png";
 
 const App = () => {
-  const services = new MarvelServices();
-  useEffect(async () => {
-    const result = await services.getCharacter("1011334");
-
-    console.log("result: ", result.data.results);
-  }, []);
   return (
     <div className="app">
       <AppHeader />
--- src/components/services/MarvelServices.js
+++ src/components/services/MarvelServices.js
@@ -1,11 +1,10 @@
 class MarvelServices {
-  baseUrl = "https://gateway.marvel.com:443/v1/public/";
+  baseUrl = "https://gateway.marvel.com:443/v1/public";
 
   apiKey = "f8a65e3990fbc2d46c94383226ba6e91";
 
-  getResource = async (url) => {
+  getResource = async (url = "") => {
     const combineUrl = `${this.baseUrl}${url}?apikey=${this.apiKey}`;
-    console.log("combineUrl: ", combineUrl);
     const res = await fetch(combineUrl);
     if (!res.ok) {
       throw new Error(`Could not fetch ${url}, status ${res.status}`);
@@ -13,12 +12,25 @@ class MarvelServices {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(this.baseUrl);
+  getAllCharacters = async () => {
+    const result = await this.getResource("/characters");
+    return result.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(`characters/${id}`);
+  getCharacter = async (id) => {
+    const result = await this.getResource(`/characters/${id}`);
+    return this._transformCharacter(result.data.results[0]);
+  };
+
+  _transformCharacter = (data) => {
+    const { name, description, thumbnail, urls } = data;
+    return {
+      name,
+      description,
+      imgUrl: `${thumbnail.path}.${thumbnail.extension}`,
+      homepage: urls[0].url,
+      wiki: urls[1].url,
+    };
   };
 }
 
